390d4dce3b319a515cb349a6d480a7e34a810057
Andrew-24coop <128892453+Andrew-24coop@users.noreply.github.com>
2025-05-04 16:37:39 +0300

    Add files via upload

---
+++ examples/ScaraMoveTo/ScaraMoveTo.ino
@@ -0,0 +1,19 @@
+#include <DualSCARA.h>
+
+GStepper2<STEPPER2WIRE> stepper1(6400, 2, 5);
+GStepper2<STEPPER2WIRE> stepper2(6400, 3, 6);
+
+DualSCARA scara(74.0, 122.0, stepper1, stepper2);
+
+void setup() {
+  Serial.begin(115200);
+  stepper1.setAcceleration(3200);
+  stepper1.setMaxSpeed(1600);
+  stepper2.setAcceleration(3200);
+  stepper2.setMaxSpeed(1600);
+
+  scara.moveTo(100, 50);
+}
+
+void loop() {
+}
